014c3e83430748f385393add3c6124ea1aaa6f12
Rakshit Singh <89170079+Bot-Rakshit@users.noreply.github.com>
2024-04-21 21:20:28 +0530

    add crown to gukesh name

--- src/components/evalbar/Evalbar.js
+++ src/components/evalbar/Evalbar.js
@@ -90,7 +90,7 @@ function EvalBar({
       return "Gorya";
     }
     if (parts.includes("Gukesh")) {
-      return "Gukesh👑";
+      return "Gukesh👑"; // final round of candidates 
     }
 
     // Find the shortest name
