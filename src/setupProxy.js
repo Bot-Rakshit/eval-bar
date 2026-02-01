9dadcd706645f098037b627303ad53cdd36360e9
Rakshit Singh <singhrakshit003@gmail.com>
2026-02-01 17:09:37 +0530

    vercel.json

---
+++ src/setupProxy.js
@@ -0,0 +1,8 @@
+// Enable SharedArrayBuffer for Stockfish 18 WASM in development
+module.exports = function(app) {
+  app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+  });
+};
